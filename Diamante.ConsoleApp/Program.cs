58f5f4ad4f122d5aa9ff1eedfb4a33d6aab91f73
Rafael Santos <rafaelsantos138@hotmail.com>
2023-03-16 13:37:15 -0300

    Add project files.

---
+++ Diamante.ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Diamante.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            bool par = true;
+
+            Console.WriteLine("-------- Diamante --------");
+            Console.WriteLine();
+
+            Console.WriteLine(">> Digite um número ímpar para formar um diamante: ");
+            Console.Write(">> ");
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            do
+            {
+                if (input % 2 == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("-------- Diamante --------");
+                    Console.WriteLine();
+                    Console.WriteLine(">> Digite um número Ímpar!");
+                    Console.Write(">> ");
+                    input = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                }
+                else
+                {
+                    par = false;
+                }
+            }
+
+            while (par == true);
+
+            for (int i = 0; i < input; i++)
+            {
+                if (i <= input / 2)
+                {
+                    Console.WriteLine(new string(' ', (input / 2) - i) + new string('x', 2 * i + 1));
+                }
+                else
+                {
+                    Console.WriteLine(new string(' ', i - (input / 2)) + new string('x', 2 * (input - i) - 1));
+                }
+            }
+        }
+    }
+}
